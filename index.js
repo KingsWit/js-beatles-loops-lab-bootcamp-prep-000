0bc57d3ab42d4e4b29b7dff9d79381273ae16d58
Taylor Tellman <taylortellman@gmail.com>
2018-05-17 16:01:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(arrayMusicians, arrayInstruments){
   var results[];
-    for (var i=0 ; i<arrayMusicians.length> ; i++){
+    for (var i = 0 ; i<arrayMusicians.length> ; i++ ){
     results.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`  ) 
     return results
     
